4cd740f5eb23cda580d59271b2c3339809ebd344
sink-opuba <39055986+sink-opuba@users.noreply.github.com>
2019-12-13 15:17:45 +0100

    set up teamwork landing page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Teamwork App</p>
-        <a
-          className="App-link"
-          href="https://github.com/sink-opuba/teamwork/tree/gh-pages"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Work in progress
+      <nav className="App-nav">
+        <a href="http://localhost:3000" className="App-link">
+          TeamWork
         </a>
+        <button>Login</button>
+      </nav>
+      <header className="App-header">
+        <h1>Let's Connect as We Work Together</h1>
+        <p>- Post Gifs, Articles, Comments and more...</p>
+        <button>Get Started</button>
       </header>
     </div>
   );
