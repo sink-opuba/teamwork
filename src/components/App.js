ed00080580c50b212ee3c41ce56d375628b32455
sink-opuba <39055986+sink-opuba@users.noreply.github.com>
2019-12-17 16:07:13 +0100

    route login and home page

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
@@ -8,12 +9,16 @@ function App() {
         <a href="http://localhost:3000" className="App-link">
           TeamWork
         </a>
-        <button>Login</button>
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
       </nav>
       <header className="App-header">
         <h1>Let's Connect as We Work Together</h1>
         <p>- Post Gifs, Articles, Comments and more...</p>
-        <button>Get Started</button>
+        <Link to="/login">
+          <button>Get Started</button>
+        </Link>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Route, BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
-// import App from "./components/App";
+import App from "./components/App";
 import Login from "./pages/login/login";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<Login />, document.getElementById("root"));
+const Routing = () => (
+  <Router>
+    <Route exact path="/" component={App} />
+    <Route path="/login" component={Login} />
+  </Router>
+);
+
+ReactDOM.render(<Routing />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
