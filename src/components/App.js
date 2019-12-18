08853c45c1476fcf6f7b3f7ca80e6067963351b2
sink-opuba <39055986+sink-opuba@users.noreply.github.com>
2019-12-18 22:05:54 +0100

    make Nav component reusable

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,12 @@
 import React from "react";
 import "./App.css";
+import Nav from "./Nav";
 import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <nav className="App-nav">
-        <a href="http://localhost:3000" className="App-link">
-          TeamWork
-        </a>
-        <Link to="/login">
-          <button>Login</button>
-        </Link>
-      </nav>
+      <Nav />
       <header className="App-header">
         <h1>Let's Connect as We Work Together</h1>
         <p>- Post Gifs, Articles, Comments and more...</p>
