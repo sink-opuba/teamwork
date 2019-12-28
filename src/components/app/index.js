32650b9bc6e3edc89aa4223dda01213c7d5b278b
sink-opuba <39055986+sink-opuba@users.noreply.github.com>
2019-12-28 10:06:22 +0100

    refactor components folder structure

---
+++ src/components/app/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./index.css";
+import Nav from "../nav";
+import { Link } from "react-router-dom";
+import { PropTypes } from "prop-types";
+
+function App({ isAuthed, logOut }) {
+  return (
+    <div className="App">
+      <Nav isLoggedIn={isAuthed} logOut={logOut} />
+      <header className="App-header">
+        <h1>Let's Connect as We Work Together</h1>
+        <p>- Post Gifs, Articles, Comments and more...</p>
+        <Link to="/user">
+          <button>Get Started</button>
+        </Link>
+      </header>
+    </div>
+  );
+}
+
+App.propTypes = {
+  isAuthed: PropTypes.bool.isRequired,
+  logOut: PropTypes.func.isRequired
+};
+
+export default App;
