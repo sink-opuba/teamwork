0d7c6b946842d83387d81d6160854bd20965255c
sink-opuba <39055986+sink-opuba@users.noreply.github.com>
2020-01-02 18:17:15 +0000

    Deploying to gh-pages from gh-pages 61742107cd4455dd9e1543bad80c813c78e7409c

---
+++ precache-manifest.a1a0a5d0c0d1dac3660b5bac68e28912.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1928b6c6dfaa1e86ca397be56e022940",
+    "url": "/teamwork/index.html"
+  },
+  {
+    "revision": "71c8ba213cfa94cd5d9d",
+    "url": "/teamwork/static/css/main.e7c0632e.chunk.css"
+  },
+  {
+    "revision": "205902d78fd11dba14a0",
+    "url": "/teamwork/static/js/2.dd599609.chunk.js"
+  },
+  {
+    "revision": "71c8ba213cfa94cd5d9d",
+    "url": "/teamwork/static/js/main.778c01c6.chunk.js"
+  },
+  {
+    "revision": "623fc6ecca87782c15ef",
+    "url": "/teamwork/static/js/runtime-main.0f497ae7.js"
+  },
+  {
+    "revision": "65bb0a158ee1967292ee4d11079d45ae",
+    "url": "/teamwork/static/media/Nunito-Regular.65bb0a15.ttf"
+  },
+  {
+    "revision": "60401ca23e69422816141e37bb836e7c",
+    "url": "/teamwork/static/media/gt-walsheim-bold.60401ca2.ttf"
+  },
+  {
+    "revision": "d47a538e8b7a588471bf01d1e91f887f",
+    "url": "/teamwork/static/media/gt-walsheim-light-oblique.d47a538e.ttf"
+  },
+  {
+    "revision": "b4dce3aa143fd23765b284a017d698ae",
+    "url": "/teamwork/static/media/gt-walsheim-regular.b4dce3aa.ttf"
+  },
+  {
+    "revision": "eb8528bcef6b654f66088344e9dd932f",
+    "url": "/teamwork/static/media/teamwork2.eb8528bc.jpg"
+  }
+]);
